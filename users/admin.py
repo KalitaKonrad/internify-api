cffa6a2f75eb500d35438ed3b236b851c8f96c1c
Konrad <konradkalita@student.agh.edu.pl>
2021-10-26 10:26:37 +0200

    feat: add support for creating users from admin panel, filtering and ordering users

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,26 @@
 from django.contrib import admin
 from .models import User
+from django.contrib.auth.admin import UserAdmin
 
-admin.site.register(User)
+
+class UserAdminConfig(UserAdmin):
+    search_fields = ('email', 'user_name')
+    list_filter = ('email', 'user_name', 'is_active', 'is_staff')
+    ordering = ('-date_joined',)
+    list_display = ('email', 'user_name', 'is_active', 'is_staff')
+
+    fieldsets = (
+        ('General information', {'fields': ('email', 'user_name')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+        ('Personal', {'fields': ('description',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'user_name', 'password1', 'password2', 'is_active', 'is_staff')}
+         ),
+    )
+
+
+admin.site.register(User, UserAdminConfig)
